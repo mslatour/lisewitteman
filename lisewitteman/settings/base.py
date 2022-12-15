f5be04b2f86c70a9cb1a8241ef4892034420e49b
Sander Latour <sanderlatour@gmail.com>
2022-12-15 16:38:54 +0100

    set X_FRAME_OPTIONS for preview feature

--- lisewitteman/settings/base.py
+++ lisewitteman/settings/base.py
@@ -165,3 +165,5 @@ WAGTAIL_SITE_NAME = "lisewitteman"
 # Base URL to use when referring to full URLs within the Wagtail admin backend -
 # e.g. in notification emails. Don't include '/admin' or a trailing slash
 WAGTAILADMIN_BASE_URL = 'http://example.com'
+
+X_FRAME_OPTIONS = 'SAMEORIGIN'
